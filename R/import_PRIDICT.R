8fad1ca77a128cbfcb3c0f9cb5de21faeb11f2a6
miahamilton5 <miahamilton5@gmail.com>
2025-07-19 18:00:13 +0000

    renamed bind_csv to import_PRIDICT

--- R/import_PRIDICT.R
+++ R/import_PRIDICT.R
@@ -10,8 +10,8 @@
 #' @param input_directory A directory.
 #' @export
 
-bind_csv <- function(input_directory) {
+import_PRIDICT <- function(input_directory) {
   af <- base::list.files(input_directory, pattern='*.csv')
   all <- data.table::rbindlist(lapply(af, function(p) { data.table::fread(paste0(input_directory, p)) }))
-  all
+  return(all)
 }
